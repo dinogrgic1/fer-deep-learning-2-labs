7de25e3a385ee48eb1c0e278be594a8d01e660fb
Dino Grgić <dino.grgic@unitfly.com>
2022-10-27 12:32:44 +0200

    Boilerplate for lab1

---
+++ lab1/models.py
@@ -0,0 +1,33 @@
+import math
+from typing import TypeAlias, Union, Collection
+
+import tensorflow as tf
+
+TFData: TypeAlias = Union[tf.Tensor, tf.Variable, float]
+
+class GMModel:
+    def __init__(self, K):
+        self.K = K
+        self.mean = tf.Variable(tf.random.normal(shape=[K]))
+        self.logvar = tf.Variable(tf.random.normal(shape=[K]))
+        self.logpi = tf.Variable(tf.zeros(shape=[K]))
+
+    @property
+    def variables(self) -> Collection[TFData]:
+        return self.mean, self.logvar, self.logpi
+
+    @staticmethod
+    def neglog_normal_pdf(x: TFData, mean: TFData, logvar: TFData):
+        var = tf.exp(logvar)
+
+        return 0.5 * (tf.math.log(2 * math.pi) + logvar + (x - mean) ** 2 / var)
+
+    @tf.function
+    def loss(self, data: TFData):
+        return NotImplemented
+
+    def p_xz(self, x: TFData, k: int) -> TFData:
+        return NotImplemented
+
+    def p_x(self, x: TFData) -> TFData:
+        return NotImplemented
