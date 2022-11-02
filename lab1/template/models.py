fd213661bd7f402f038b1db374857aa05464684f
Dino Grgić <dino.grgic@unitfly.com>
2022-11-02 10:29:06 +0100

    Cnt

--- lab1/template/models.py
+++ lab1/template/models.py
@@ -1,10 +1,9 @@
 import math
 from typing import Union, Collection
-from typing_extensions import TypeAlias
 
 import tensorflow as tf
 
-TFData: TypeAlias = Union[tf.Tensor, tf.Variable, float]
+TFData: Union[tf.Tensor, tf.Variable, float]
 
 class GMModel:
     def __init__(self, K):
@@ -20,20 +19,15 @@ class GMModel:
     @staticmethod
     def neglog_normal_pdf(x: TFData, mean: TFData, logvar: TFData):
         var = tf.exp(logvar)
+
         return 0.5 * (tf.math.log(2 * math.pi) + logvar + (x - mean) ** 2 / var)
 
     @tf.function
     def loss(self, data: TFData):
-        return -tf.math.reduce_logsumexp(-((GMModel.neglog_normal_pdf(data, self.mean, self.logvar) + tf.math.reduce_logsumexp(self.logpi, 0) - self.logpi)), 1)
-    
+        raise NotImplementedError
+
     def p_xz(self, x: TFData, k: int) -> TFData:
-        var = tf.exp(self.logvar[k])
-        return (1 / tf.math.sqrt(2 * math.pi * var)) * tf.exp(-0.5 * (((x - self.mean[k]) ** 2) / var))
+        raise NotImplementedError
 
     def p_x(self, x: TFData) -> TFData:
-        pi = tf.nn.softmax(self.logpi)
-        whole = tf.convert_to_tensor(0.0)
-        for pi_, k in zip(pi, range(self.K)):
-            values = pi_ * self.p_xz(x, k)
-            whole = tf.add(values, whole)
-        return whole
+        raise NotImplementedError
